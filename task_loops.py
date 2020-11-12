18e956bd4b652a327b2c6cce5a56a615b616687a
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-12 11:50:59 +0000

    new task with comprehension completed, added comment with format/syntax of the list comprehension

--- task_loops.py
+++ task_loops.py
@@ -3,6 +3,8 @@
 
 ages = [5, 15, 64, 27, 84, 26]
 
+
+# [output     for loop      conditional]
 odd_ages = [age for age in ages if age % 2 != 0] 
 print(odd_ages)
 
@@ -13,10 +15,12 @@ chicken_names = ["Hen Solo", "Cluck Norris", "Hennifer Lopez", "ChewPekka", "Fea
 # Return a new list with chickens whose name is more than 10 characters
 # Return a list of chickens whose name stars with the letter H
 
+# [output     for loop      conditional]
 long_names = [chicken for chicken in chicken_names if len(chicken) > 10] 
 print(long_names)
 
 
+# [output     for loop      conditional]
 start_names_H = [chicken for chicken in chicken_names if chicken[0] == 'H'] 
 print(start_names_H)
 
